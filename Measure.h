60ae8296f979c71788833d24381702f3cb366873
fxy <fxy233@gmail.com>
2020-05-06 15:54:05 +0200

    Create Measure.h

---
+++ Measure.h
@@ -0,0 +1,88 @@
+/*************************************************************************
+                           User  -  description
+                             -------------------
+    début                : $DATE$
+    copyright            : (C) $YEAR$ par $AUTHOR$
+    e-mail               : $EMAIL$
+*************************************************************************/
+
+//---------- Interface de la classe <Measure> (fichier Measure.h) ----------------
+#if ! defined ( Measure_H )
+#define Measure_H
+
+using namespace std;
+//--------------------------------------------------- Interfaces utilisées
+#include <string>
+
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+//------------------------------------------------------------------------
+// Rôle de la classe <AttributeMeasure>
+//
+//
+//------------------------------------------------------------------------
+
+class Measure
+{
+//----------------------------------------------------------------- PUBLIC
+
+public:
+//----------------------------------------------------- Méthodes publiques
+    // type Méthode ( liste des paramètres );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+
+//------------------------------------------------- Surcharge d'opérateurs
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+
+//-------------------------------------------- Constructeurs - destructeur
+    Measure ( const Measure & unMeasure );
+    // Mode d'emploi (constructeur de copie) :
+    //
+    // Contrat :
+    //
+
+    Measure ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+    
+    Measure (string timestamp,string value,bool falseData );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+    virtual ~Measure ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+//------------------------------------------------------------------ PRIVE
+
+protected:
+//----------------------------------------------------- Méthodes protégées
+
+//----------------------------------------------------- Attributs protégés
+    string timestamp;
+    string value;
+    bool falseData;
+
+};
+
+//-------------------------------- Autres définitions dépendantes de <AttributeMeasure>
+
+#endif // AttributeMeasure_H
+
+
