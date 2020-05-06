2066b53f897c0556e452f5a20d3e0634824d1171
Camilo Sierra <camilo.sierra.r@gmail.com>
2020-05-06 16:50:37 +0200

    Sensor et Makefile

---
+++ Sensor.h
@@ -0,0 +1,88 @@
+/*************************************************************************
+                           Sensor  -  description
+                             -------------------
+    début                : $06/05/2020$
+    copyright            : (C) $2020$ par $gtutunaru$
+    e-mail               : $gheorghe.tutunaru@insa-lyonfr$
+*************************************************************************/
+
+//---------- Interface de la classe <Sensor> (fichier Sensor.h) ----------------
+#if ! defined ( Sensor_H )
+#define Sensor_H
+
+//--------------------------------------------------- Interfaces utilisées
+using namespace std;
+#include <iostream>
+#include <string>
+
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+//------------------------------------------------------------------------
+// Rôle de la classe <Sensor>
+//
+//
+//------------------------------------------------------------------------
+
+class Sensor
+{
+//----------------------------------------------------------------- PUBLIC
+
+public:
+//----------------------------------------------------- Méthodes publiques
+    // type Méthode ( liste des paramètres );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+    bool viewQuality(double lat, double long, string date_start, string date_end="");
+
+    bool login(string mail, string password);
+
+    bool logout();
+
+//------------------------------------------------- Surcharge d'opérateurs
+    Sensor & operator = ( const Sensor & unSensor );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+
+//-------------------------------------------- Constructeurs - destructeur
+    Sensor ( const Sensor & unSensor );
+    // Mode d'emploi (constructeur de copie) :
+    //
+    // Contrat :
+    //
+
+    Sensor (string _mail, string _password, string _prenom, string _nom );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+    virtual ~Sensor ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+//------------------------------------------------------------------ PRIVE
+
+protected:
+//----------------------------------------------------- Méthodes protégées
+
+//----------------------------------------------------- Attributs protégés
+    int sensorId;
+    double latitude;
+    double longitude;
+    string description;
+
+};
+
+//-------------------------------- Autres définitions dépendantes de <Sensor>
+
+#endif // Sensor_H
