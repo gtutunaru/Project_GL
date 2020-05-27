980b66c4b199301db2b9cb9e88aebc74410c8349
gtutunaru <47929798+gtutunaru@users.noreply.github.com>
2020-05-27 16:57:50 +0200

    push

--- Data.cpp
+++ Data.cpp
@@ -379,6 +379,8 @@ void viewQuality(double c_lat, double c_long, double radius, tm time){
     //for (const auto & i : goodMeasures) {
             
     //}
+}
+
 void checkImpact ( int cleanId ) const
 {
 
