39dd30b30a38f5abc8815204e69530159b9f2e51
Camilo Sierra <camilo.sierra.r@gmail.com>
2020-06-01 22:40:26 +0200

    checkImpactValue

--- Data.cpp
+++ Data.cpp
@@ -142,7 +142,7 @@ bool Data::filterData(int id) {
             sensor = part->getSensor();
             int nbSensor = nbSensorInArea(sensor->getLatitude(),sensor->getLongitude(),radius);
 
-            //Calcul of the smallest radius with at least 5 sensors inside 
+            //Calcul of the smallest radius with at least 5 sensors inside
             while(nbSensor<5) {
                 radius +=10;
                 nbSensor = nbSensorInArea(sensor->getLatitude(),sensor->getLongitude(),radius);
@@ -932,7 +932,7 @@ void Data::checkImpactRadius (  int cleanId, int nbDays )
     struct tm beforeStart =startDate; //the day right before start of cleaner
     DatePlusDays(&beforeStart, -1);
 
-    
+
     //cout<<asctime(&afterDate)<<endl;
     cout<<"Information about the cleaner"<<endl<<endl;
     cout<<cleaners[cleanId]->toString()<<endl;
@@ -942,12 +942,12 @@ void Data::checkImpactRadius (  int cleanId, int nbDays )
 
         //Quality before
         double * before = viewQuality(cleaners[cleanId]->getLatitude(), cleaners[cleanId]->getLongitude(), r, beforeDate, beforeStart);
-        
+
         //Quality After
         double * after = viewQuality(cleaners[cleanId]->getLatitude(), cleaners[cleanId]->getLongitude(), r, startDate, endDate);
 
         //Impact
-        
+
 
         if (before[4]>0 && after[4]>0) {
             counter++;
@@ -964,10 +964,10 @@ void Data::checkImpactRadius (  int cleanId, int nbDays )
         if(!isImpact && counter>1)
         {
             cout<<"There is a significant impact on Radius : "<<r<<" km"<<endl<<endl;
-            /*cout<<"Difference O3 : "<<impact[0]<<" µg/m3"<<endl;
+            cout<<"Difference O3 : "<<impact[0]<<" µg/m3"<<endl;
             cout<<"Difference SO2 : "<<impact[1]<<" µg/m3"<<endl;
             cout<<"Difference NO2 : "<<impact[2]<<" µg/m3"<<endl;
-            cout<<"Difference PM10 : "<<impact[3]<<" µg/m3"<<endl;*/
+            cout<<"Difference PM10 : "<<impact[3]<<" µg/m3"<<endl;
             cout<<"Difference ATMO : "<<impact[4]<<endl;
         }
         if (r<100) {
@@ -1001,73 +1001,68 @@ void Data::checkImpactRadius (  int cleanId, int nbDays )
 
 void Data::checkImpactValue ( int cleanId, int nbDays, double r)
 {
-    bool isImpact = false;
     double impact[5];
-    cout<<"So far so good 3"<<endl;
+    bool isImpact = false;
+
     struct tm startDate; //start day of cleaner working
-    string s = "2019-01-31 12:00:00";
-    string s2 = "2019-02-28 12:00:00";
-    strptime(s.c_str(), "%Y-%m-%d %H:%M:%S", &startDate);
+    strptime(cleaners[cleanId]->getStart().c_str(), "%Y-%m-%d %H:%M:%S", &startDate);
 
     struct tm endDate; //last day of cleaner working
-    strptime(s2.c_str(), "%Y-%m-%d %H:%M:%S", &endDate);
+    strptime(cleaners[cleanId]->getEnd().c_str(), "%Y-%m-%d %H:%M:%S", &endDate);
+    DatePlusDays(&endDate, -1); //to have last day of cleaner working, since it stops at 00:00 the next day
+
+    struct tm beforeDate =startDate; //first day of period before cleaner
+    DatePlusDays(&beforeDate, -nbDays);
+
+    struct tm beforeStart =startDate; //the day right before start of cleaner
+    DatePlusDays(&beforeStart, -1);
 
 
-    /*struct tm beforeDate =startDate; //pour ajouter des jours, faut utiliser comme ca
-    DatePlusDays(&beforeDate, -nbDays);*/
-
-    //cout<<endDate<<endl;
-    //cout<<startDate<<endl;
-
-    /*struct tm afterDate =endDate;
-    DatePlusDays(&afterDate, nbDays);*/
     //cout<<asctime(&afterDate)<<endl;
-    //cout<<asctime(&endDate)<<endl;
+    cout<<"Information about the cleaner"<<endl<<endl;
+    cout<<cleaners[cleanId]->toString()<<endl;
+
 
     //Quality before
-    double * before = viewQuality(cleaners[cleanId]->getLatitude(), cleaners[cleanId]->getLongitude(), r, startDate);
+    double * before = viewQuality(cleaners[cleanId]->getLatitude(), cleaners[cleanId]->getLongitude(), r, beforeDate, beforeStart);
+
     //Quality After
     double * after = viewQuality(cleaners[cleanId]->getLatitude(), cleaners[cleanId]->getLongitude(), r, startDate, endDate);
 
-    cout<<"Before : "<<r<<" km"<<endl<<endl;
-    cout<<"O3 : "<<before[0]<<endl;
-    cout<<"SO2 : "<<before[1]<<endl;
-    cout<<"NO2 : "<<before[2]<<endl;
-    cout<<"PM10 : "<<before[3]<<endl<<endl;
-
-    cout<<"After : "<<r<<" km"<<endl<<endl;
-    cout<<"O3 : "<<after[0]<<endl;
-    cout<<"SO2 : "<<after[1]<<endl;
-    cout<<"NO2 : "<<after[2]<<endl;
-    cout<<"PM10 : "<<after[3]<<endl<<endl;
-
     //Impact
-    //cout<<"Test before"<<endl;
-    for (int i = 0; i<5;i++)
-    {
-        //cout<<before[i]<<endl;
-        //cout<<after[i]<<endl;
-        if (before[i]>=0 && after[i]>=0){
-            impact[i]= after[i]-before[i];
-            //cout<<before[i]<<" "<<after[i]<<endl;
+
+
+    if (before[4]>0 && after[4]>0) {
+        isImpact = (after[4]+4<before[4]);
+        if (isImpact)
+        {
+            for (int i = 0; i<5;i++)
+            {
+                impact[i]= after[i]-before[i];
+            }
         }
+    } else {
+        cout<<"No sensors in this area, please try with a bigger radius"<<endl;
+        return;
     }
 
-    if (before[4]>=0 && after[4]>=0) {
-        isImpact = after[4]!=before[4];
+    if(isImpact)
+    {
+        cout<<"There is a significant impact on Radius : "<<r<<" km"<<endl<<endl;
+        cout<<"Difference O3 : "<<impact[0]<<" µg/m3"<<endl;
+        cout<<"Difference SO2 : "<<impact[1]<<" µg/m3"<<endl;
+        cout<<"Difference NO2 : "<<impact[2]<<" µg/m3"<<endl;
+        cout<<"Difference PM10 : "<<impact[3]<<" µg/m3"<<endl;
+        cout<<"Difference ATMO : "<<impact[4]<<endl;
     }
 
-    cout<<"isImpact : "<<isImpact<<endl;
-    cout<<"On a radius of "<<r<<" km the impact is :"<<endl<<endl;
-    /*cout<<"Difference O3 : "<<impact[0]<<endl;
-    cout<<"Difference SO2 : "<<impact[1]<<endl;
-    cout<<"Difference NO2 : "<<impact[2]<<endl;
-    cout<<"Difference PM10 : "<<impact[3]<<endl;*/
-    cout<<"Difference Atmos : "<<impact[4]<<endl;
-
-    cout<<"finished value"<<endl;
     delete[]before;
     delete[]after;
+
+    if (!isImpact){
+        cout<<"There is no impact from this cleaner in this area, but maybe on a smaller area it does."<<endl;
+        cout<<"Reminder : Within the app you can check the maximum radius of impact of this cleaner"<<endl;
+    }
 }
 
 //-------------------------------------------- Constructeurs - destructeur
