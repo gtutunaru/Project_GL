423d59e6140c08c996e68ffc5d6bed3c2326fc0b
gtutunaru <47929798+gtutunaru@users.noreply.github.com>
2020-06-03 14:02:55 +0200

    last commit

--- main.cpp
+++ main.cpp
@@ -162,7 +162,6 @@ int main(){
     d.readParticulars("dataset/users.csv");
     d.readMeasures("./dataset/measurements.csv");
 
-    cout<<d.AttributesToString()<<endl;
 
     bool exit = false;
     bool exitGov =false;
